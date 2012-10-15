84e876fc8c3f1a2c15647188c9ff75fc28199af4
Christopher Budge <Christopher.Budge@mtctrains.com>
2012-10-15 14:01:42 -0600

    pg. 305

--- src/com/example/glbasics/FirstTriangleTest.java
+++ src/com/example/glbasics/FirstTriangleTest.java
@@ -21,16 +21,18 @@ public class FirstTriangleTest extends GLGame
 	{
 		GLGraphics glGraphics;
 		FloatBuffer vertices;
+		final int VERTEX_SIZE = (2+2) * 4;
+		
 		public FirstTriangleScreen(Game game) 
 		{
 			super(game);
-			glGraphics = ((GLGame)game).getGLGraphics();
-			ByteBuffer byteBuffer = ByteBuffer.allocateDirect(3 * 2 * 4);
+			glGraphics = ((GLGame)game).getGLGraphics();			
+			ByteBuffer byteBuffer = ByteBuffer.allocateDirect(3 * VERTEX_SIZE);
 			byteBuffer.order(ByteOrder.nativeOrder());
 			vertices = byteBuffer.asFloatBuffer();
-			vertices.put( new float[] { 0.0f, 0.0f,
-			319.0f, 0.0f,
-			160.0f, 479.0f});
+			vertices.put( new float[] { 0.0f, 0.0f,0.0f,1.0f,
+										319.0f, 0.0f,1.0f,1.0f,
+										160.0f, 479.0f, 0.5f,0.0f});
 			vertices.flip();
 		}
 		
@@ -43,9 +45,13 @@ public class FirstTriangleTest extends GLGame
 			gl.glMatrixMode(GL10.GL_PROJECTION);
 			gl.glLoadIdentity();
 			gl.glOrthof(0, 320, 0, 480, 1, -1);
-			gl.glColor4f(1, 0, 0, 1);
+//			gl.glColor4f(1, 0, 0, 1);
 			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glVertexPointer( 2, GL10.GL_FLOAT, 0, vertices);
+			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+			vertices.position(0);			
+			gl.glVertexPointer( 2, GL10.GL_FLOAT, VERTEX_SIZE, vertices);
+			vertices.position(2);
+			gl.glColorPointer(4, GL10.GL_FLOAT, VERTEX_SIZE, vertices);			
 			gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
 		}
 		
